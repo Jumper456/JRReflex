82aec869bcca884cf68df4675d323ae0cd235b00
Jumper456 <jumper456@gmail.com>
2024-09-01 21:52:30 +0200

    Removed unused code, autofomated, added comments, version tested/ working well

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,63 @@
+// 2024 J.K.Wachowicz
+// This program is provided under GPL 3.0 licence, you can find the licence file in the root folder of this project
+// This program measures human reaction (response) time
+// Developed on the "ESP32-C3 super mini" board, but shuould run on any Arduino compatible board/MCU
+// check pins your harware uses (button, led) before running
+// using MCU with no operating system should give exact values without significant delays caused by hardware or OS
+
 #include <Arduino.h>
 
+// define harware wiring and logic
 #define LED_PIN 8
 #define BUTTON_PIN 9
-
 #define LED_ON 0
 #define LED_OFF 1
 #define BUTTON_ON 0
 #define BUTTON_OFF 1
 
-void setup() {
+// standard Arduino setup, which runs only once after MCU starts or resets
+void setup()
+{
+  // initialize serial (UART) needed to write results too as well as debugging info if needed
   Serial.begin(9600);
-  Serial.println("setup entered");
+
+  // setup the wiring
   pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
-  Serial.println("setup finished");
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // say hallo
+  Serial.println("JRReflex ready, just press Boot button after blue LED is up, as quickly as you can");
 }
 
-//uint8_t ledState = 1;
-bool counting = false;
-unsigned long startTime = 0;
-//unsigned long endTime = 0;
-unsigned long delayTime = 0;
+// global varibles needed to hold values between consequtive loop() calls/runs
+bool counting = false;       // says if time counint is started or not
+unsigned long startTime = 0; // moment in time, that counting started (in ms since device has stared)
 
-void loop() {
-  if (!counting){
-    // set led OFF, 
-    digitalWrite(LED_PIN,LED_OFF);
-    //randomize delay, 
-    delay(random(2000,5000));
-    //after delay set led ON
-    digitalWrite(LED_PIN,LED_ON);
-    //remember start time time
+void loop()
+{
+  if (!counting)
+  {
+    // set led OFF,
+    digitalWrite(LED_PIN, LED_OFF);
+    // randomize delay,
+    delay(random(2000, 5000));
+    // after delay set led ON
+    digitalWrite(LED_PIN, LED_ON);
+    // remember start time time
     startTime = millis();
-    //set mode to counting reaction time
+    // set mode to "started counting reaction time"
     counting = true;
   }
-  else{
-    //check if button pressed
-    if (digitalRead(BUTTON_PIN) == BUTTON_ON){
-      //if button pressed display reaction time
+  else
+  {
+    // now we are waiting until human presses the button in the reaction of the led has been turned on
+    // check if button pressed
+    if (digitalRead(BUTTON_PIN) == BUTTON_ON)
+    {
+      // display reaction time
       Serial.printf("Reaction time in miliseconds %d \n", millis() - startTime);
-      //start again
+      // start again
       counting = false;
     }
   }
-
-
-  //Serial.println("loop entered");
-  //Serial.printf("Miliseconds %d \n", millis());
-  //digitalWrite(LED_PIN, ledState);
-  //ledState = ledState xor (u_int8_t)1;
-  //Serial.printf("Pin %d value %d \n", BUTTON_PIN, digitalRead(BUTTON_PIN));
-  //delay(1000);
-  //Serial.printf("ledState=%d\n", ledState);
-  //Serial.println("loop finished");
 }
-
